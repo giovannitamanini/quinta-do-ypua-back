ac795ae504d52c4756b58fa3c901afe998cfd120
Giovanni Tamanini <giovanni.tnini@gmail.com>
2024-10-27 11:22:28 -0300

    Atualizando documentação da AcomodacaoController

--- src/main/java/com/pousada/controller/AcomodacaoController.java
+++ src/main/java/com/pousada/controller/AcomodacaoController.java
@@ -21,39 +21,39 @@ public class AcomodacaoController {
     @Autowired
     private final AcomodacaoService acomodacaoService;
 
-    @Operation(summary = "Cadastra uma nova acomodação")
+    @Operation(summary = "Cadastra uma nova acomodação", method = "POST")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public AcomodacaoDTO criarAcomodacao(@RequestBody AcomodacaoDTO acomodacaoDTO) {
         return acomodacaoService.criarAcomodacao(acomodacaoDTO);
     }
 
-    @Operation(summary = "Atualiza o cadastro de uma acomodação")
+    @Operation(summary = "Atualiza o cadastro de uma acomodação", method = "PUT")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public AcomodacaoDTO atualizarAcomodacao(@RequestBody AcomodacaoDTO acomodacaoDTO) {
         return acomodacaoService.atualizarAcomodacao(acomodacaoDTO);
     }
 
-    @Operation(summary = "Busca acomodações da pousada a partir do Id da acomodação", method = "GET")
+    @Operation(summary = "Faz a exclusão do cadastro de uma acomodação", method = "DELETE")
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarAcomodacaoPorId(@PathVariable Integer id) {
+        acomodacaoService.deletarAcomodacaoPorId(id);
+    }
+
+    @Operation(summary = "Busca acomodação a partir do Id", method = "GET")
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public AcomodacaoDTO buscarAcomodacaoPorId(@PathVariable Integer id) {
         return acomodacaoService.buscarAcomodacaoPorId(id);
     }
 
-    @Operation(summary = "Busca todas as acomodações da pousada", method = "GET")
+    @Operation(summary = "Busca todas as acomodações", method = "GET")
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
     public List<AcomodacaoDTO> buscarTodasAcomodacoes() {
        return acomodacaoService.buscarTodasAcomodacoes();
-    }
-
-    @Operation(summary = "Faz a exclusão do cadastro de uma acomodação")
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletarAcomodacaoPorId(@PathVariable Integer id) {
-        acomodacaoService.deletarAcomodacaoPorId(id);
     }
 
 }

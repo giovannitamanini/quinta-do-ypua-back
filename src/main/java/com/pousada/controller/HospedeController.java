69e655c95a2e62370815a57bdf4c4c15176c6cc0
Giovanni Tamanini <giovanni.tnini@gmail.com>
2024-10-27 11:40:50 -0300

    Atualizando documentação do HospedeController. Retirando comentários desnecessários e organizando o código

--- src/main/java/com/pousada/controller/HospedeController.java
+++ src/main/java/com/pousada/controller/HospedeController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,52 +18,46 @@ public class HospedeController {
 
     private final HospedeService hospedeService;
 
-    @Operation(summary = "Cadastra um novo hóspede")
+    @Operation(summary = "Cadastra um novo hóspede", method = "POST")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public HospedeDTO criarHospede(@RequestBody HospedeDTO hospedeDTO) {
         return hospedeService.criarHospede(hospedeDTO);
     }
 
-    @Operation(summary = "Atualiza o cadastro de um hóspede")
+    @Operation(summary = "Atualiza o cadastro de um hóspede", method = "PUT")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public HospedeDTO atualizarHospede(@RequestBody HospedeDTO hospedeDTO) {
         return hospedeService.atualizarHospede(hospedeDTO);
     }
 
-    @Operation(summary = "Busca hóspede pelo Id de registro")
+    @Operation(summary = "Faz a exclusão do cadastro de um hóspede", method = "DELETE")
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarHospedePorId(@PathVariable Long id) {
+        hospedeService.deletarHospedePorId(id);
+    }
+
+    @Operation(summary = "Busca hóspede a partir do Id", method = "GET")
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public HospedeDTO buscarHospedePorId(@PathVariable Long id) {
         return hospedeService.buscarHospedePorId(id);
     }
 
-    @Operation(summary = "Busca hóspedes pelo primeiro nome")
+    @Operation(summary = "Busca hóspedes pelo nome", method = "GET")
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
     public List<HospedeDTO> buscarHospedesPorNome(@RequestParam(value = "nome") String nome) {
         return hospedeService.buscarHospedesPorNome(nome);
     }
 
-    @Operation(summary = "Busca todos os hóspedes cadastrados")
+    @Operation(summary = "Busca todos os hóspedes", method = "GET")
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
     public List<HospedeDTO> buscarTodosHospedes() {
         return hospedeService.buscarTodosHospedes();
     }
 
-//    @Operation(summary = "Faz a exclusão do cadastro do hóspede")
-//    @DeleteMapping("/{id}")
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    public ResponseEntity<Object> deletarHospedePorId(@PathVariable Long id) {
-//        return hospedeService.deletarHospedePorId(id);
-//    }
-
-    @Operation(summary = "Faz a exclusão do cadastro do hóspede")
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletarHospedePorId(@PathVariable Long id) {
-        hospedeService.deletarHospedePorId(id);
-    }
 }

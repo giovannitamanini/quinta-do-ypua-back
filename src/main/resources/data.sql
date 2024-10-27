bb81f1878d8cf3cd608fedbce176d35df72c303c
Giovanni Tamanini <giovanni.tnini@gmail.com>
2024-10-27 11:03:37 -0300

    Atualizando inserções no banco de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,17 @@
-INSERT IGNORE INTO informacao_pousada (nome, endereco_1, endereco_2, cep, telefone, email, web_site, facebook, instagram, whatsapp)
-VALUES ("Pousada Quinta do Ypuã", "Estrada Ipua, nº 6", "Laguna - SC", "88790-000", "(48) 99940-9732", "pousadaquintadoypua@gmail.com", "https://www.quintadoypua.com", "https://www.facebook.com/pousadaquintadoypua", "https://www.instagram.com/pousadaquintadoypua/", "+55 48 99940-9732");
+INSERT IGNORE INTO acomodacao (id, nome, valor_diaria, quantidade_hospedes)
+VALUES (1, "Domo", 590.00, 2);
 
-INSERT IGNORE INTO acomodacao (id, nome, valor_diaria, quantidade_hospedes, descricao_camas)
-VALUES (1, "Domo", 590.00, 2, "1 Casal");
+INSERT IGNORE INTO acomodacao (id, nome, valor_diaria, quantidade_hospedes)
+VALUES (2, "Charrua (Bus)", 490.00, 2);
 
-INSERT IGNORE INTO acomodacao (id, nome, valor_diaria, quantidade_hospedes, descricao_camas)
-VALUES (2, "Charrua (Bus)", 490.00, 2, "1 Casal");
+INSERT IGNORE INTO acomodacao (id, nome, valor_diaria, quantidade_hospedes)
+VALUES (3, "Suíte com Cozinha", 390.00, 3);
 
-INSERT IGNORE INTO acomodacao (id, nome, valor_diaria, quantidade_hospedes, descricao_camas)
-VALUES (3, "Suíte com Cozinha", 390.00, 3, "1 Solteiro, 1 Casal");
+INSERT IGNORE INTO acomodacao (id, nome, valor_diaria, quantidade_hospedes)
+VALUES (4, "Chalé Família", 590.00, 5);
 
-INSERT IGNORE INTO acomodacao (id, nome, valor_diaria, quantidade_hospedes, descricao_camas)
-VALUES (4, "Chalé Família", 590.00, 5, "1 Solteiro, 2 Casal");
-
-INSERT IGNORE INTO acomodacao (id, nome, valor_diaria, quantidade_hospedes, descricao_camas)
-VALUES (5, "Cabana", 490.00, 3, "1 Solteiro, 1 Casal");
+INSERT IGNORE INTO acomodacao (id, nome, valor_diaria, quantidade_hospedes)
+VALUES (5, "Cabana", 490.00, 3);
 
 INSERT IGNORE INTO acomodacao (id, nome, valor_diaria)
 VALUES (6, "Estacionamento para Overlands", 100.00);

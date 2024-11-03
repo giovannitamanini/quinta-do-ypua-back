6a988b4843ab2a4cc6512aa7db5efbd431e76509
Giovanni Tamanini <giovanni.tnini@gmail.com>
2024-11-03 08:41:01 -0300

    Corrigindo schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS hospede (
     email VARCHAR(100) NOT NULL UNIQUE,
     telefone VARCHAR(15),
     cep VARCHAR(10),
-    logradouro VARCHAR(100), //logradouro, bairro
+    logradouro VARCHAR(100),
     bairro VARCHAR(100),
     cidade VARCHAR(45),
     estado VARCHAR(45),
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS acomodacao (
     data_atualizacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS comodidade ( //comodidade talvez
+CREATE TABLE IF NOT EXISTS comodidade (
     id INT PRIMARY KEY AUTO_INCREMENT,
     descricao VARCHAR(100) NOT NULL,
     tipo ENUM('MOBÍLIA', 'ELETRODOMÉSTICOS', 'UTENSÍLIOS', 'OUTROS') NOT NULL,
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS acomodacao_comodidade (
     id_comodidade INT NOT NULL,
     qtd_comodidade INT NOT NULL CHECK (qtd_comodidade > 0),
     data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    data_atualizacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    data_atualizacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (id_acomodacao) REFERENCES acomodacao(id),
     FOREIGN KEY (id_item) REFERENCES comodidade(id)
 );
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS reserva (
     data_reserva DATE NOT NULL,
     qtd_diarias INT NOT NULL CHECK (qtd_diarias > 0),
     valor_total DECIMAL(12,2) NOT NULL,
-    status_reserva ENUM('PENDENTE', 'EM ANDAMENTO', 'FINALIZADA', 'CANCELADA') NOT NULL,
+    status_reserva ENUM('PENDENTE', 'EM_ANDAMENTO', 'FINALIZADA', 'CANCELADA') NOT NULL,
     status_pagamento ENUM('PENDENTE', 'PAGO', 'CANCELADO') NOT NULL,
     data_check_in DATE,
     data_check_out DATE,

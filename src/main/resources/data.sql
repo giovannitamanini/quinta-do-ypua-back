bb3a481ff0c068f6d222eee15df8f96a213ffdca
Giovanni Tamanini <giovanni.tnini@gmail.com>
2024-11-15 04:07:33 -0300

    Atualizando dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -25,11 +25,10 @@ VALUES ('GERENTE');
 INSERT IGNORE INTO permissao (nome)
 VALUES ('ADMIN');
 
-INSERT IGNORE INTO usuario (usuario, senha, ativo) VALUES ('patrick', '12345', 1);
-INSERT IGNORE INTO usuario (usuario, senha, ativo) VALUES ('alex', '23456', 1);
-INSERT IGNORE INTO usuario (usuario, senha, ativo) VALUES ('john', '34567', 1);
+INSERT IGNORE INTO usuario (usuario, senha, ativo) VALUES ('patrick', '$2a$10$cTUErxQqYVyU2qmQGIktpup5chLEdhD2zpzNEyYqmxrHHJbSNDOG.', 1);
+INSERT IGNORE INTO usuario (usuario, senha, ativo) VALUES ('alex', '$2a$10$.tP2OH3dEG0zms7vek4ated5AiQ.EGkncii0OpCcGq4bckS9NOULu', 1);
+INSERT IGNORE INTO usuario (usuario, senha, ativo) VALUES ('john', '$2a$10$E2UPv7arXmp3q0LzVzCBNeb4B4AtbTAGjkefVDnSztOwE7Gix6kea', 1);
 
 INSERT IGNORE INTO usuario_permissao (id_usuario, id_permissao) VALUES (1, 1);
 INSERT IGNORE INTO usuario_permissao (id_usuario, id_permissao) VALUES (2, 2);
 INSERT IGNORE INTO usuario_permissao (id_usuario, id_permissao) VALUES (3, 3);
-

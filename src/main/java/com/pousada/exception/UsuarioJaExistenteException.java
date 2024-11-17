b4b4bc6169faed587c156ca13ea95bb96bf9d779
maria <maria.alicemp@hotmail.com>
2024-11-17 20:09:13 -0300

    ajustes maria

--- src/main/java/com/pousada/exception/UsuarioJaExistenteException.java
+++
@@ -1,9 +0,0 @@
-package com.pousada.exception;
-
-public class UsuarioJaExistenteException extends RuntimeException {
-
-    public UsuarioJaExistenteException(String mensagem) {
-        super(mensagem);
-    }
-
-}
